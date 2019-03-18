da23d9a4b50bf5a1e23fcb790bfaae629cd75da5
janryWang <wangzhili56@126.com>
2019-03-18 22:49:53 +0800

    update

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,13 @@ export default styled(({ dataSource, className }) => {
       <div className="header-content">
         <Link to={`/`}>
           <div className="logo">
-            {React.isValidElement(logo) ? logo : logo ? <img src={logo} /> : 'This is Logo'}
+            {React.isValidElement(logo) ? (
+              logo
+            ) : logo ? (
+              <img src={logo} />
+            ) : (
+              'This is Logo'
+            )}
           </div>
         </Link>
         <Search {...search} />
@@ -21,46 +27,52 @@ export default styled(({ dataSource, className }) => {
     </div>
   )
 })`
-border-bottom: 1px solid rgb(238, 238, 238);
-background:#fff;
-margin-bottom: 20px;
-box-shadow: 0 0 10px #eeeeeec9;
-.header-content {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 0 30px;
-  @media (max-width: 860px) {
-    margin: 0 20px;
-  }
-  @media (max-width: 690px) {
-    margin: 0 10px;
-  }
-  .logo {
-    height: 60px;
+  border-bottom: 1px solid rgb(238, 238, 238);
+  background: #fff;
+  margin-bottom: 20px;
+  box-shadow: 0 0 10px #eeeeeec9;
+  .header-content {
     display: flex;
     align-items: center;
-    position:relative;
-    justify-content: left;
-    width: 270px;
+    justify-content: space-between;
+    margin: 0 30px;
     @media (max-width: 860px) {
-      width:180px;
+      margin: 0 20px;
+    }
+    @media (max-width: 690px) {
+      margin: 0 10px;
+    }
+    .logo {
+      height: 60px;
+      display: flex;
+      align-items: center;
+      position: relative;
+      justify-content: left;
+      width: 270px;
+      a {
+        font-size: 20px;
+        font-weight: 300;
+        text-transform: uppercase;
+      }
+    }
+    @media (max-width: 860px) {
+      width: 180px;
     }
     img {
       height: 60px;
       display: inline-block;
     }
-    &:after{
-      content:"";
-      display:block;
-      height:22px;
-      border-left:1px solid #eee;
-      position:absolute;
-      top:50%;
-      right:0;
-      transform:translateY(-50%);
+    &:after {
+      content: '';
+      display: block;
+      height: 22px;
+      border-left: 1px solid #eee;
+      position: absolute;
+      top: 50%;
+      right: 0;
+      transform: translateY(-50%);
       @media (max-width: 690px) {
-        display:none;
+        display: none;
       }
     }
   }
